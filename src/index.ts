87e5016272d19e352a9b5df6be8472b366f97c04
4ebuard <yartspetr@gmail.com>
2023-05-21 22:53:03 +0300

    added nodemon port 3000 productTitle

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,7 @@ import express, {Request, Response} from 'express'
 //create express app
 
 const app = express()
-const port = process.env.PORT
+const port = process.env.PORT || 3000
 
 const helloMessage = [{title: 'Hello world'}]
 const products = [{title: 'tomato'}, {title: 'orange'}]
@@ -14,6 +14,10 @@ app.get('/', (req: Request, res: Response) => {
 })
 app.get('/products', (req: Request, res: Response) => {
     res.send(products)
+})
+app.get('/products/:productTitle', (req: Request, res: Response) => {
+  let product = products.find(p => p.title === req.params.productTitle)
+  res.send(product)
 })
 
 app.get('/addresses', (req: Request, res: Response) => {
