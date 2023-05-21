27cca1737973dea7ed8e4fbf63b8f31539817402
4ebuard <yartspetr@gmail.com>
2023-05-21 21:35:30 +0300

    added produscts and addresses

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,22 @@
 import express, {Request, Response} from 'express'
 
+//create express app
+
 const app = express()
 const port = process.env.PORT
 
-app.get('/', (req: Request, res: Response) => {
-  let helloMessage = 'I can deploy!!! yeah'
-    res.send(helloMessage)
+const products = [{title: 'tomato'}, {title: 'orange'}]
+const addresses = [{value: 'Lenina 5'}, {value: 'panina 10'}] 
+
+app.get('/products', (req: Request, res: Response) => {
+    res.send(products)
 })
+
+app.get('/addresses', (req: Request, res: Response) => {
+  res.setDefaultEncoding(addresses)
+})
+
+//start app
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
