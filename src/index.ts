bf7699266cd34cadce6f41c9f5916dd56a85b6d7
4ebuard <yartspetr@gmail.com>
2023-05-21 21:44:32 +0300

    added helloMessage

--- src/index.ts
+++ src/index.ts
@@ -5,9 +5,13 @@ import express, {Request, Response} from 'express'
 const app = express()
 const port = process.env.PORT
 
+const helloMessage = [{title: 'Hello world'}]
 const products = [{title: 'tomato'}, {title: 'orange'}]
 const addresses = [{value: 'Lenina 5'}, {value: 'panina 10'}] 
 
+app.get('/', (req: Request, res: Response) => {
+  res.send(helloMessage)
+})
 app.get('/products', (req: Request, res: Response) => {
     res.send(products)
 })
