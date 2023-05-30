55d944027f7dec9fb211f9921375f6bb1f466065
4ebuard <yartspetr@gmail.com>
2023-05-30 23:48:19 +0300

    06 added layers for products

--- src/repositories/products-repository.ts
+++ src/repositories/products-repository.ts
@@ -1,1 +1,45 @@
+const products = [{id: 101, title: 'tomato'}, {id: 102, title: 'orange'}]
 
+export const productsRepository = {
+    findProducts (title: string | null | undefined) {
+        if (title) {
+            let filteredProducts = products.filter(p => p.title.indexOf(title) > -1)
+            return filteredProducts
+          } else {
+            return products
+          } 
+    },
+    createProducts(title: string ) {
+        const newProduct = { 
+            id: +(new Date()),
+            title: title
+          }
+          products.push(newProduct)
+            return newProduct
+    },
+    findProductById(id: number) {
+        let product = products.find(p => p.id === id)
+        return product;
+    },
+    updateProduct(id: number, title: string) {
+        let product = products.find(p => p.id === id)
+        if (product) {
+          product.title = title
+          return true;
+        }
+        else {
+          return false;
+     }
+    },
+    deleteProduct(id: number) {
+        for (let i = 0; i < products.length; i++) {
+            if (products[i].id === id) {
+                products.splice(i, 1)
+                return true;
+            } else {
+                return false;
+            }
+        }
+    }
+     
+}
